aad0cbc9295b680fcb0806c64ccc46c8acd25609
Boingoloid <matthew.acalin@gmail.com>
2016-11-13 11:55:54 -0800

    h

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,45 @@
+import os
+
+
+
+WSGI_APPLICATION = 'app.wsgi.application'
+DEBUG = True
+
+PROJECT_FOLDER = os.path.realpath(os.path.dirname(__file__))
+BASE_DIR = os.path.join(os.path.dirname(__file__), '..')
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'app.views'
+
 DJANGO_SETTINGS_MODULE = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -14,4 +56,3 @@ DJANGO_SETTINGS_MODULE = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
