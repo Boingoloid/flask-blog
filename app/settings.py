6b5602408c2d4e6d61accf68e5e7b63f2863b991
Boingoloid <matthew.acalin@gmail.com>
2016-11-13 11:46:37 -0800

    h

--- app/settings.py
+++ app/settings.py
@@ -1,1 +1,17 @@
-#
+DJANGO_SETTINGS_MODULE = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': TEMPLATE_DIRS,
+        'APP_DIRS': True,
+        'OPTIONS': {'debug':DEBUG,
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'app.wsgi.application'
