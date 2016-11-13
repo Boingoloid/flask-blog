b2249701780613780915c90be483985f48e4ee84
Boingoloid <matthew.acalin@gmail.com>
2016-11-13 12:56:40 -0800

    f

--- app/wsgi.py
+++ app/wsgi.py
@@ -6,7 +6,7 @@ import sys
 # as well as any WSGI server configured to use this file.
 
 # sys.executable = '/usr/local/python-2.7.2/bin/python'
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
